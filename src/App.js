96a7fdf735f2fa1b336a4cda708696ca20b8dcb6
shravanr3ddy <90720113+shravanr3ddy@users.noreply.github.com>
2023-04-20 03:40:17 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import ChatBot from './Components/chatbot';
 import ChatBox from './Components/chatbox';
 
 function App() {
   return (
     <div className="App">
       <ChatBox />
-       {/* <ChatBot /> */}
     </div>
   );
 }
